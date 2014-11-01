a560862b8a6d35782f12d15a9f69bd37bc25ecd1
Travis Cardwell <travis.cardwell@extellisys.com>
2014-11-01 17:39:15 +0900

    refactor content rules

--- site.hs
+++ site.hs
@@ -1,7 +1,6 @@
 module Main where
 
 -- [HP] http://hackage.haskell.org/package/base
-import Control.Applicative ((<$>))
 import Data.Monoid ((<>), mempty)
 
 -- [HP] http://hackage.haskell.org/package/filepath
@@ -20,8 +19,8 @@ import qualified Text.HTML.TagSoup as TS (Tag(TagOpen))
 -- configuration
 
 -- | Site configuration: used to define the default context
-siteConfig :: [(String, String)]
-siteConfig =
+siteConfig :: Context String
+siteConfig = toContext
     [ ("sTitle",       "Keep Pushing the Limits")
     , ("mAuthor",      "Rob Nugen, <hakyll.robnugen.com@robnugen.com>")
     , ("mDescription", "blatherings of Rob Nugen; don't read unless bored.")
@@ -48,14 +47,29 @@ feedConfig subtitle = FeedConfiguration
     , feedRoot        = "http://hakyll.robnugen.com"
     }
 
+-- | Title of index.html
+indexTitle :: String
+indexTitle = "Blog"
+
+-- | Number of post teasers included on index.html
+indexNumPosts :: Int
+indexNumPosts = 10
+
+-- | Number of post teasers included in the feed.
+feedNumPosts :: Int
+feedNumPosts = 15
+
 ------------------------------------------------------------------------------
 -- main
 
 main :: IO ()
 main = hakyllWith hakyllConfig $ do
+    -- Compile templates.
+    match "templates/*" $ compile templateCompiler
+
     -- Copy static files into the root.
     match "static/*" $ do
-      route $ gsubRoute "^static/" (const "")
+      route $ routeRemove "^static/"
       compile copyFileCompiler
 
     -- Copy image files.
@@ -68,91 +82,110 @@ main = hakyllWith hakyllConfig $ do
       route idRoute
       compile compressCssCompiler
 
-    -- Compile each blog post and save a teaser for later use.
-    match (posts .||. travels .||. "*.markdown") $ do
-        route   $ gsubRoute "posts/" (const "") `composeRoutes`
-                  markdownToFolderRoute
-        compile $ pandocCompiler
-          >>= return . fmap (demoteHeaders . mkPreFocusable)
-          >>= saveSnapshot "content"
-          >>= loadAndApplyTemplate "templates/post.html"    dateCtx
-          >>= saveSnapshot "post"
-          >>= loadAndApplyTemplate "templates/default.html" defaultCtx
-          >>= removeIndexHtml
-          >>= relativizeUrls
+    -- Compile blog posts, saving teasers in snapshot "posts".
+    match "posts/**" $ do
+      route $ routeRemove "^posts/" `composeRoutes` mdToDirRoute
+      compile $ pandocCompiler
+        >>= return . fmap (demoteHeaders . makePreFocusable)
+        >>= saveSnapshot "posts"
+        >>= loadAndApplyTemplate "templates/post.html" (dateCtx <> defaultCtx)
+        >>= loadAndApplyTemplate "templates/default.html" defaultCtx
+        >>= removeIndexHtml
+        >>= relativizeUrls
 
-    -- With the teasers, create an rss feed for the most recent 15 posts.
-    create ["recent.rss"] $ do
-        route idRoute
-        compile $ loadAllSnapshots posts "content"
-          >>= (take 15 <$>) . recentFirst
-          >>= renderAtom (feedConfig "All posts")
-                  (teaserField "description" "content"
-                <> bodyField "description" 
-                <> defaultContext)
-          >>= removeIndexHtml
+    -- Compile travels content.
+    match "travels/**" $ do
+      route mdToDirRoute
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultCtx
+        >>= removeIndexHtml
+        >>= relativizeUrls
 
-    -- With all posts in a list, create the archive. TODO: paginate.
-    create ["archive/index.html"] $ do
-        route idRoute
-        compile $ makeItem ("" :: String)
-          >>= loadAndApplyTemplate "templates/archive.html"
-              (listField "posts" dateCtx
-                (loadAllSnapshots posts "content" >>= recentFirst)
-              <> archiveCtx)
-          >>= loadAndApplyTemplate "templates/default.html"
-                (archiveCtx <> defaultCtx)
-          >>= removeIndexHtml
-          >>= relativizeUrls
+    -- Compile root content.
+    match ("about.markdown" .||. "contact.markdown" .||.
+           "search.markdown" .||. "travels.markdown") $ do
+      route mdToDirRoute
+      compile $ pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultCtx
+        >>= removeIndexHtml
+        >>= relativizeUrls
 
-    -- Compile the latest 10 posts into teasers. TODO: paginate.
+    -- Create index.html using the latest post teasers.
     create ["index.html"] $ do
-        route idRoute
-        compile $ makeItem ("" :: String)
-          >>= loadAndApplyTemplate "templates/index.html"
-              (listField "posts" (dateCtx <> teaserCtx)
-                (loadAllSnapshots posts "content" >>= recentFirst)
-                <> constField "index" "true"
-                <> constField "title" "Home")
-          >>= loadAndApplyTemplate "templates/default.html"
-                  (constField "title" "Blog" <> dateCtx <> defaultCtx)
-          >>= removeIndexHtml
-          >>= relativizeUrls
+      route idRoute
+      compile $ makeItem ("" :: String)
+        >>= loadAndApplyTemplate "templates/index.html"
+              (listField "posts" (teaserCtx <> dateCtx <> defaultCtx)
+                 (loadAllSnapshots "posts/**" "posts"
+                    >>= fmap (take indexNumPosts) . recentFirst))
+        >>= loadAndApplyTemplate "templates/default.html"
+              (constField "title" indexTitle <> dateCtx <> defaultCtx)
+        >>= removeIndexHtml
+        >>= relativizeUrls
 
-    -- Compile the templates
-    match "templates/*" $ compile templateCompiler
+    -- Create archive/index.html showing all posts.  (temporary)
+    create ["archive/index.html"] $ do
+      route idRoute
+      compile $ makeItem ("" :: String)
+        >>= loadAndApplyTemplate "templates/archive.html"
+              (listField "posts" (dateCtx <> defaultCtx)
+                 (loadAllSnapshots "posts/**" "posts" >>= recentFirst)
+               <> archiveCtx)
+        >>= loadAndApplyTemplate "templates/default.html"
+              (archiveCtx <> defaultCtx)
+        >>= removeIndexHtml
+        >>= relativizeUrls
+
+    -- Create recent.rss using lastest post teasers.
+    create ["recent.rss"] $ do
+      route idRoute
+      compile $ loadAllSnapshots "posts/**" "posts"
+        >>= fmap (take feedNumPosts) . recentFirst
+        >>= renderAtom (feedConfig "All posts")
+              (teaserField "description" "posts"
+               <> bodyField "description"
+               <> defaultContext)
+        >>= removeIndexHtml
+
   where
-    travels   = "travels/**.markdown"
-    posts      = "posts/**.markdown"
-    archiveCtx = constField  "title"  "Archives"
-    dateCtx    = dateField   "date"   "%B %e, %Y"
-                 <> dateField "Tdate" "%Y-%m-%d"
-                 <> defaultContext
-    defaultCtx = listToFields siteConfig          <> defaultContext
-    teaserCtx  = teaserField "teaser" "content"   <> defaultContext
+    defaultCtx, dateCtx, teaserCtx, archiveCtx :: Context String
+    defaultCtx = siteConfig <> defaultContext
+    dateCtx    = dateField "date" "%B %e, %Y" <> dateField "Tdate" "%Y-%m-%d"
+    teaserCtx  = teaserField "teaser" "content"
+    archiveCtx = constField "title" "Archive"
 
--- | Make all <pre>'s focusable by adding the attribute tabindex="0".
-mkPreFocusable :: String -> String
-mkPreFocusable = withTags process
+------------------------------------------------------------------------------
+-- utility functions
+
+-- | Create a Context String from a list of (String, String).
+toContext :: [(String, String)] -> Context String
+toContext ((k, v):xs) = constField k v <> toContext xs
+toContext _           = mempty
+
+-- | Remove part of a path from a route.
+routeRemove :: String -> Routes
+routeRemove s = gsubRoute s (const "")
+
+-- | Route Markdown files to an index.html file in a directory.
+mdToDirRoute :: Routes
+mdToDirRoute = gsubRoute ".markdown" (const "/index.html")
+
+-- | Make all pre tags focusable by adding the attribute tabindex="0".
+makePreFocusable :: String -> String
+makePreFocusable = withTags process
   where
     process :: TS.Tag String -> TS.Tag String
     process (TS.TagOpen "pre" a) = TS.TagOpen "pre" $ ("tabindex", "0") : a
     process t                    = t
 
-listToFields :: [(String, String)] -> Context String
-listToFields ((k, v):xs) = constField k v <> listToFields xs
-listToFields _           = mempty
-
--- | Replace .markdown with /index.html, creating a folder for the page.
-markdownToFolderRoute :: Routes
-markdownToFolderRoute = gsubRoute ".markdown" (const "/index.html")
-
 -- | Remove index.html from all links.
 removeIndexHtml :: Item String -> Compiler (Item String)
-removeIndexHtml = return . (withUrls removeIndexStr <$>)
-  where removeIndexStr :: String -> String
-        removeIndexStr u = case splitFileName u of
-            (d, "index.html") | isLocal d -> d
-            _                             -> u
-        isLocal :: String -> Bool
-        isLocal = not . isInfixOf "://"
+removeIndexHtml = return . fmap (withUrls process)
+  where
+    process :: String -> String
+    process u = case splitFileName u of
+      (d, "index.html") | isLocal d -> d
+      _                             -> u
+
+    isLocal :: String -> Bool
+    isLocal = not . isInfixOf "://"
